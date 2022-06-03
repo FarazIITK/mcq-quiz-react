e052a0338ffd45cb3d4888ffe9d5cc2b0339f1c3
fhusain01 <fhusain@adaptready.com>
2022-06-04 01:12:53 +0530

    Added logic to display options, score and continue button

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,56 @@ function App() {
 
   const [currentQuestion, setCurrentQuestion] = useState<number>(1);
   const [isTestCompleted, setIsTestCompleted] = useState<boolean>(false);
+  const [correctAnswers, setCorrectAnswers] = useState<number>(0);
+  const [incorrectAnswers, setIncorrectAnswers] = useState<number>(0);
+  const [isAnswerGiven, setIsAnswerGiven] = useState<boolean>(false);
 
-  const handleContinueButton = () => {
+  const optionButtonHandler = (e: any) => {
+    console.log(e.target.innerText);
+    setIsAnswerGiven(true);
+  };
+
+  const continueButtonHandler = () => {
     const newQuestion = currentQuestion + 1;
     if (newQuestion > questions.length) {
       setIsTestCompleted(true);
     } else {
       setCurrentQuestion(newQuestion);
+      setIsAnswerGiven(false);
     }
+  };
+
+  const shuffleArray = (array: string[]) => {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+
+  const renderOptions = () => {
+    const options = [
+      questions[currentQuestion - 1].correct_answer,
+      ...questions[currentQuestion - 1].incorrect_answers
+    ];
+    console.log('Options: ', options);
+    const shuffledOptions = shuffleArray(options);
+
+    return (
+      <div>
+        {shuffledOptions.map((element) => {
+          return (
+            <button disabled={isAnswerGiven} onClick={optionButtonHandler}>
+              {decodeURIComponent(element)}
+            </button>
+          );
+        })}
+        {/* <h2>vdkjvkj</h2>
+        <h2>dbjb</h2> */}
+      </div>
+    );
   };
 
   return (
@@ -34,14 +76,38 @@ function App() {
           <h1>
             Question {currentQuestion} of {questions.length}
           </h1>
-          <h3>
-            {questions[currentQuestion - 1].category.replaceAll('%', ' ')}
-          </h3>
+          <h3>{decodeURIComponent(questions[currentQuestion - 1].category)}</h3>
           <h3>
             Difficulty:{' '}
-            {questions[currentQuestion - 1].difficulty.replaceAll('%', ' ')}
+            {decodeURIComponent(questions[currentQuestion - 1].difficulty)}
           </h3>
-          <button onClick={handleContinueButton}>Continue</button>
+          <p>
+            <b>Question:</b>
+            {decodeURIComponent(questions[currentQuestion - 1].question)}
+          </p>
+          {renderOptions()}
+          {/* <button onClick={optionButtonHandler} disabled={isAnswerGiven}>
+            Option 1
+          </button>
+          <button onClick={optionButtonHandler} disabled={isAnswerGiven}>
+            Option 1
+          </button>
+          <button onClick={optionButtonHandler} disabled={isAnswerGiven}>
+            Option 1
+          </button>
+          <button onClick={optionButtonHandler} disabled={isAnswerGiven}>
+            Option 1
+          </button> */}
+
+          {isAnswerGiven && (
+            <button onClick={continueButtonHandler}>Continue</button>
+          )}
+
+          <br />
+          <br />
+          <br />
+          <h4>Correct Answers: {correctAnswers}</h4>
+          <h4>Incorrect Answers: {incorrectAnswers}</h4>
         </div>
       )}
       {isTestCompleted && (
