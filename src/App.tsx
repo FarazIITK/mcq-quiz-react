26f3e26f7aa72166e5eb2399ec5a9e1514ed6c3a
fhusain01 <fhusain@adaptready.com>
2022-06-03 23:42:06 +0530

    Added logic to display question title and continue button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+interface IQuestionData {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: [string, string, string];
+}
+
 function App() {
+  const questions: IQuestionData[] = require('./Assets/question.json');
+
+  const [currentQuestion, setCurrentQuestion] = useState<number>(1);
+  const [isTestCompleted, setIsTestCompleted] = useState<boolean>(false);
+
+  const handleContinueButton = () => {
+    const newQuestion = currentQuestion + 1;
+    if (newQuestion > questions.length) {
+      setIsTestCompleted(true);
+    } else {
+      setCurrentQuestion(newQuestion);
+    }
+  };
+
   return (
     <div className="App">
       <h1>MCQ Quiz</h1>
+
+      {!isTestCompleted && (
+        <div>
+          <h1>
+            Question {currentQuestion} of {questions.length}
+          </h1>
+          <h3>
+            {questions[currentQuestion - 1].category.replaceAll('%', ' ')}
+          </h3>
+          <h3>
+            Difficulty:{' '}
+            {questions[currentQuestion - 1].difficulty.replaceAll('%', ' ')}
+          </h3>
+          <button onClick={handleContinueButton}>Continue</button>
+        </div>
+      )}
+      {isTestCompleted && (
+        <div>
+          <h1>Test Completed</h1>
+        </div>
+      )}
     </div>
   );
 }
